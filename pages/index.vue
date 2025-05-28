71573fb9c6787658d44fe748eb063e080cd60c18
joseRvas13 <jose.vasquez111@soy.sena.edu.co>
2025-05-27 22:30:37 -0500

    se deja el index como principal y se corrigen errores para ya dejar la page lista

--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,112 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
+  <v-app>
+    <!-- Fondo con imagen y overlay oscuro -->
+    <div class="background">
+      <div class="overlay d-flex align-center justify-center fill-height">
+        <v-container>
+          <v-row justify="center">
+            <v-col cols="12" md="8" lg="6">
+              <v-card class="glass pa-6" elevation="10">
+                <v-card-title class="justify-center white--text encabezado">
+                  Bienvenido a ReceFy
+                </v-card-title>
+
+                <v-card-subtitle class="text-center mb-3 white--text">
+                  <v-icon class="mr-2" color="white">mdi-silverware-fork-knife</v-icon>
+                  Cocina con lo que tienes en casa
+                </v-card-subtitle>
+                <v-card-text>
+                  <v-form>
+                    <v-text-field v-model="formLogin.correo_electronico" dense outlined color="white"
+                      label="Correo Electrónico" prepend-inner-icon="mdi-email" class="white-text"></v-text-field>
+
+                    <v-text-field v-model="formLogin.contraseña" dense outlined color="white" label="Contraseña"
+                      type="password" prepend-inner-icon="mdi-lock"></v-text-field>
+                  </v-form>
+                </v-card-text>
+
+                <v-card-actions class="justify-center">
+                  <v-btn color="green lighten-1" dark @click="goToRecetas">
+                    <v-icon left>mdi-login</v-icon> Iniciar Sesión
+                  </v-btn>
+
+                  <v-btn color="blue lighten-1" dark>
+                    <v-icon left>mdi-account-plus</v-icon> Crear Cuenta
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
       </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+    </div>
+  </v-app>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  name: 'IndexPage',
+  data() {
+    return {
+      formLogin: {
+        correo_electronico: '',
+        contraseña: '',
+      },
+      ping: [],
+    }
+  },
+
+  mounted() {
+    this.traerDjango();
+  },
+
+  methods: {
+    goToRecetas() {
+      this.$router.push({ name: 'Recetas' });
+    },
+
+    async traerDjango() {
+      try {
+        const response = await this.$axios.get('index');
+        this.ping = response.data;
+        this.$toast.error('¡ Recefy no se encuentra Disponible ! ')
+      } catch (error) {
+        console.error({error});
+        this.$toast.error(error);
+
+      }
+    }
+
   }
 }
 </script>
+
+<style scoped>
+@import 'typeface-lobster';
+
+.background {
+  background-image: url('https://images.pexels.com/photos/842545/pexels-photo-842545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+  position: relative;
+}
+
+.overlay {
+  background: rgba(0, 0, 0, 0.6);
+  backdrop-filter: blur(4px);
+}
+
+.encabezado {
+  font-size: 35px;
+  font-family: 'Lobster', cursive;
+}
+
+.glass {
+  background: rgba(255, 255, 255, 0.1);
+  border-radius: 20px;
+  backdrop-filter: blur(10px);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  color: white;
+}
+</style>
